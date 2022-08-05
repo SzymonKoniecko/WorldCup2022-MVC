a5b4182aec8445e80114fce1331f1f807b1aebdc
Szymon Koniecko <80451@student.pb.edu.pl>
2022-08-05 22:14:01 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WorldCup2022_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/GroupStage.cs
@@ -0,0 +1,9 @@
+﻿namespace WorldCup2022_MVC.Models
+{
+    public class GroupStage
+    {
+        public int groupStageID { get; set; }
+        public string home { get; set; }
+        public string away { get; set; }
+    }
+}
