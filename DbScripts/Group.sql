3c81aedc506cf871e8991c59e45cf6e685902303
Szymon Koniecko <80451@student.pb.edu.pl>
2022-08-17 19:27:26 +0200

    ticket #2

---
+++ DbScripts/Group.sql
@@ -0,0 +1,38 @@
+﻿SET IDENTITY_INSERT [WorldCup2022].[dbo].[Group] ON
+INSERT INTO [WorldCup2022].[dbo].[Group]  (
+       [groupId]
+      ,[name]
+)
+VALUES
+    (
+        1,
+        'A'
+    ),
+    (
+        2,
+        'B'
+    ),
+    (
+        3,
+        'C'
+    ),
+    (
+        4,
+        'D'
+    ),
+    (
+        5,
+        'E'
+    ),
+    (
+        6,
+        'F'
+    ),
+    (
+        7,
+        'G'
+    ),
+    (
+        8,
+        'H'
+    );
