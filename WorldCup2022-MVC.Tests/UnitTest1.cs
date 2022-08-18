129426653af76cb34d85102ca1533eb03000f29e
Szymon Koniecko <80451@student.pb.edu.pl>
2022-08-19 00:30:49 +0200

    Tests to repo ticket #11

---
+++ WorldCup2022-MVC.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace WorldCup2022_MVC.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
