506bfa0f564743c5d35a0a2b96b32b605a89d9f0
Szymon Koniecko <80451@student.pb.edu.pl>
2022-10-17 00:43:52 +0200

    #21 Added winner etc.

--- Controllers/KnockoutStageController.cs
+++ Controllers/KnockoutStageController.cs
@@ -27,14 +27,29 @@ namespace WorldCup2022_MVC.Controllers
             var json_teams = _promotedteamsservice.GetAllPromotedTeams(id);
             var teams = JsonConvert.DeserializeObject<TeamVM[]>(json_teams);
             ViewBag.teams = teams;
-            MatchVM[] matchesVM = new MatchVM[16];
+            MatchKnockoutStageVM[] matchesKSVM = new MatchKnockoutStageVM[8];
+            string[] winners = new string[8];
+            string[] placesInKnockoutStage = new string[32];
+            WinnerVM[] winnersKSVM = new WinnerVM[16];
+            int j = 0;
+            for (int i = 0; i < 16; i++)
+            {
+                placesInKnockoutStage[j] = matches[i].home;
+                j++;
+                placesInKnockoutStage[j] = matches[i].away;
+                j++;
+            }
             for (int i = 0; i < 8; i++)
             {
-                matchesVM[i] = SimulateMatchForKnockoutPhase(i, matches);
+                (matchesKSVM[i], winnersKSVM[i]) = SimulateMatchForKnockoutPhase(i, matches, placesInKnockoutStage);
             }
-            return View(matchesVM);
+            for (int i = 0; i < 8; i++)
+            {
+                Console.WriteLine(winnersKSVM[i].teamPlaceInGroup + " " + winnersKSVM[i].newPlaceInKnockoutStage);
+            }
+            return View(matchesKSVM);
         }
-        private MatchVM SimulateMatchForKnockoutPhase(int i, KnockoutStageVM[] knockoutStageVMs)
+        private (MatchKnockoutStageVM, WinnerVM) SimulateMatchForKnockoutPhase(int i, KnockoutStageVM[] knockoutStageVMs, string[] places)
         {
             Random random = new Random();
             int hOppor = random.Next(0, 7);
@@ -43,28 +58,65 @@ namespace WorldCup2022_MVC.Controllers
             float aEff = (float)(random.Next(0, 100) * 0.01);
             int homeGoals = (int)Math.Floor(hOppor * hEff);
             int awayGoals = (int)Math.Floor(aOppor * aEff);
-            bool isHomeWinner = false;
-            if (homeGoals > awayGoals)
+            int homeGoals_draw = 0;
+            int awayGoals_draw = 0;
+            bool isDraw = false;
+            string winner = "";
+            string newPlaceInKnockoutStage = "";
+            if (homeGoals == awayGoals)
             {
-                isHomeWinner = true;
+                isDraw = true;
+                (homeGoals_draw, awayGoals_draw) = PenaltyKicks();
+                if (homeGoals_draw > awayGoals_draw)
+                {
+                    winner = knockoutStageVMs[i].home;
+                    newPlaceInKnockoutStage = places[i + 16];
+                }
+                else
+                {
+                    winner = knockoutStageVMs[i].away;
+                    newPlaceInKnockoutStage = places[i + 16];
+                }
             }
             else
             {
-                isHomeWinner = false;
+                if (homeGoals > awayGoals)
+                {
+                    winner = knockoutStageVMs[i].home;
+                    newPlaceInKnockoutStage = places[i + 16];
+                }
+                else
+                {
+                    winner = knockoutStageVMs[i].away;
+                    newPlaceInKnockoutStage = places[i + 16];
+                }
+                isDraw = false;
             }
-            var match = new MatchVM()
+            var match = new MatchKnockoutStageVM()
             {
-                homeOpportunities = hOppor,
-                awayOpportunities = aOppor,
-                homeEfficiency = hEff,
-                awayEfficiency = aEff,
                 homeGoals = (int)Math.Floor(hOppor * hEff),
                 awayGoals = (int)Math.Floor(aOppor * aEff),
-                isHomeWinner = isHomeWinner,
+                isDraw = isDraw,
                 homePlaceInGroup = knockoutStageVMs[i].home,
-                awayPlaceInGroup = knockoutStageVMs[i].away
+                awayPlaceInGroup = knockoutStageVMs[i].away,
+                homeGoals_draw = homeGoals_draw,
+                awayGoals_draw = awayGoals_draw,
             };
-            return match;
+            var winner_team = new WinnerVM() {
+                teamPlaceInGroup = winner,
+                newPlaceInKnockoutStage = newPlaceInKnockoutStage
+            };
+
+            return (match, winner_team);
+        }
+        private (int, int) PenaltyKicks()
+        {
+            Random random = new Random();
+            float hEff = (float)(random.Next(0, 100) * 0.01);
+            float aEff = (float)(random.Next(0, 100) * 0.01);
+            int homeGoals_draw = (int)Math.Floor(5 * hEff);
+            int awayGoals_draw = (int)Math.Floor(5 * aEff);
+            return (homeGoals_draw, awayGoals_draw);
         }
     }
 }
---
+++ ViewModels/WinnerVM.cs
@@ -0,0 +1,8 @@
+﻿namespace WorldCup2022_MVC.ViewModels
+{
+    public class WinnerVM
+    {
+        public string teamPlaceInGroup { get; set; }
+        public string newPlaceInKnockoutStage { get; set; }
+    }
+}
